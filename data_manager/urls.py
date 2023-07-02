3c479e76f5096ff194493fa8f447f8dd88a9858c
Philip Otieno <iamphilipotieno@gmail.com>
2023-07-03 02:46:10 +0300

    feat: add category details views

--- data_manager/urls.py
+++ data_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CategoryListCreateAPIView, CategoryRetrieveUpdateDestroyAPIView
+from .views import CategoryListCreateAPIView, CategoryRetrieveUpdateDestroyAPIView, CategoryDetailsCreateAPIView
 
 urlpatterns = [
     path('category/', CategoryListCreateAPIView.as_view(), name='category-list-create'),
     path('category/<int:pk>/', CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-retrieve-update-destroy'),
+    path('category_details/<int:category_id>/', CategoryDetailsCreateAPIView.as_view(), name='category-details'),
 ]
--- data_manager/views.py
+++ data_manager/views.py
@@ -3,6 +3,7 @@ from .models import Category, CategoryDetails
 from .serializers import CategorySerializer, CategoryDetailsSerializer
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
 
 
@@ -10,10 +11,35 @@ class CategoryListCreateAPIView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+    def list(self, request, *args, **kwargs):
+        categories = self.get_queryset()
+        serializer = self.get_serializer(categories, many=True)
+        data = []
+        for category in categories:
+            category_details = CategoryDetails.objects.filter(category=category)
+            details_serializer = CategoryDetailsSerializer(category_details, many=True)
+            category_data = {
+                'category': serializer.data,
+                'category_details': details_serializer.data
+            }
+            data.append(category_data)
+        return Response(data)
 
 class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        category_details = CategoryDetails.objects.filter(category=instance)
+        details_serializer = CategoryDetailsSerializer(category_details, many=True)
+        data = {
+            'category': serializer.data,
+            'category_details': details_serializer.data
+        }
+        return Response(data)
+
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -21,39 +47,17 @@ class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView
         return Response({"message": "Category deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 
-# class CategoryListCreateView(viewsets.ModelViewSet):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
+class CategoryDetailsCreateAPIView(generics.CreateAPIView):
+    queryset = CategoryDetails.objects.all()
+    serializer_class = CategoryDetailsSerializer
 
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         headers = self.get_success_headers(serializer.data)
-#         return Response(serializer.data, status=201, headers=headers)
+    def get_category(self, category_id):
+        try:
+            return Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            raise NotFound(detail="Category not found.")
 
-class CategoryDetailsViewSet(viewsets.ModelViewSet):
-    pass
-#     serializer_class = CategoryDetailsSerializer
-
-#     def get_queryset(self):
-#         category_id = self.request.query_params.get('category_id')
-#         if category_id:
-#             queryset = CategoryDetails.objects.filter(category_id=category_id)
-#         else:
-#             queryset = CategoryDetails.objects.all()
-#         return queryset
-
-#     def create(self, request, *args, **kwargs):
-#         category_id = request.query_params.get('category_id')
-#         try:
-#             category = Category.objects.get(id=category_id)
-#         except Category.DoesNotExist:
-#             return Response({'error': 'Category not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-#         request.data['category'] = category_id
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         headers = self.get_success_headers(serializer.data)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def perform_create(self, serializer):
+        category_id = self.kwargs['category_id']
+        category = self.get_category(category_id)
+        serializer.save(category=category)
