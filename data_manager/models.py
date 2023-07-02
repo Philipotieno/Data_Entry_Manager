70d1cf34d0642dc1e31dad35e1271c9ef221320a
Philip Otieno <iamphilipotieno@gmail.com>
2023-07-02 18:58:13 +0300

    feat: add models

--- data_manager/models.py
+++ data_manager/models.py
@@ -4,7 +4,16 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField()
-    address = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+    
+
+class CategoryDetails(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    employees = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.category.name} - Details"
